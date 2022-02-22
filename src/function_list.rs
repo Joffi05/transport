37eab8a2339c87a536bee5f5f01ecd635d96bc02
Jonathan <joffi0566@gmail.com>
2022-02-22 16:46:04 +0100

    working with vec

--- src/function_list.rs
+++ src/function_list.rs
@@ -2,7 +2,7 @@ use std::{error::Error, ops::Index, rc::Rc};
 use crate::socket::Sendable;
 
 pub struct Funnel {
-    funcs: Vec<Rc<dyn Fn(Box<dyn Sendable>) -> Box<dyn Sendable>>>,
+    funcs: Vec<Rc<dyn Fn(Vec<u8>) -> Vec<u8>>>,
     current: u32,
 }
 
@@ -14,24 +14,24 @@ impl Funnel {
         }
     }
 
-    pub fn next(&mut self) -> Result<Rc<dyn Fn(Box<dyn Sendable>) -> Box<dyn Sendable>>, Box<dyn Error>> {
+    pub fn next(&mut self) -> Result<Rc<dyn Fn(Vec<u8>) -> Vec<u8>>, Box<dyn Error>> {
         self.current += 1;
         Ok(self.funcs[self.current as usize].clone())
     }
 
-    pub fn current(&mut self) -> Result<Rc<dyn Fn(Box<dyn Sendable>) -> Box<dyn Sendable>>, Box<dyn Error>> {
+    pub fn current(&mut self) -> Result<Rc<dyn Fn(Vec<u8>) -> Vec<u8>>, Box<dyn Error>> {
         Ok(self.funcs[self.current as usize].clone())
     }
 
-    pub fn add_fn(&mut self, fun: Box<dyn Fn(Box<dyn Sendable>) -> Box<dyn Sendable>>) {
+    pub fn add_fn(&mut self, fun: Box<dyn Fn(Vec<u8>) -> Vec<u8>>) {
         self.funcs.push(Rc::new(fun))
     }
 
-    pub fn funnel_through(&mut self, start_val: Box<dyn Sendable>) -> Box<dyn Sendable> {
-        let mut last: Box<dyn Sendable> = self.funcs[0](start_val);
+    pub fn funnel_through(&mut self, start_val: Vec<u8>) -> Vec<u8> {
+        let mut last = self.funcs[0](start_val);
 
         Box::new(for (i, e) in self.funcs.iter().enumerate() {
-            if i >= 2 {
+            if i >= 1 {
                 last = e(last)
             }
         });
@@ -56,10 +56,14 @@ mod tests {
         type FunnelFn = fn(dyn Sendable) -> dyn Sendable;
 
         let mut funnel = Funnel::new();
-            funnel.add_fn(shortener as Box<dyn Fn(Box<(dyn Sendable + 'static)>) -> Box<(dyn Sendable + 'static)>>);
+        funnel.add_fn(Box::new(shortener));
+        funnel.add_fn(Box::new(shortener));
+        funnel.add_fn(Box::new(shortener));
+        let result = funnel.funnel_through(test_vec);
+        assert_eq!(result, vec![0,0])
     }
 
-    fn shortener(mut vec: Vec<u8>) -> impl Sendable {
+    fn shortener(mut vec: Vec<u8>) -> Vec<u8> {
         match vec.pop() {
             Some(x) => {},
             None => panic!("scheise")
